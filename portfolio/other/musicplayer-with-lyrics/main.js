d7df26c778478bf20b8f8cc6ac213c055703605f
hyzuru <asatenshi@gmail.com>
2019-07-21 18:15:29 +0900

    musicplayer

---
+++ portfolio/other/musicplayer-with-lyrics/main.js
@@ -0,0 +1,622 @@
+var lyricsText = [
+  {
+    text: "",
+    timeIn: "00:00.00",
+    duration: 11000
+  },
+  {
+    text: "I think I'm drowning",
+    timeIn: "00:10.80",
+    duration: 2200
+  },
+  {
+    text: "Asphyxiated",
+    timeIn: "00:13.00",
+    duration: 1290
+  },
+  {
+    text: "I want to break the spell",
+    timeIn: "00:14.90",
+    duration: 2300
+  },
+  {
+    text: "That you've created",
+    timeIn: "00:17.00",
+    duration: 2400
+  },
+  {
+    text: "You're something beautiful",
+    timeIn: "00:18.98",
+    duration: 2000
+  },
+  {
+    text: "A contradiction",
+    timeIn: "00:20.94",
+    duration: 2010
+  },
+  {
+    text: "I want to play the game",
+    timeIn: "00:23.00",
+    duration: 2070
+  },
+  {
+    text: "I want the friction",
+    timeIn: "00:25.00",
+    duration: 1600
+  },
+  {
+    text: "You will be ",
+    timeIn: "00:26.86",
+    duration: 3200
+  },
+  {
+    text: "the death of me",
+    timeIn: "00:30.00",
+    duration: 3950
+  },
+  {
+    text: "Yeah, you will be",
+    timeIn: "00:35.46",
+    duration: 4320
+  },
+  {
+    text: "the death of me",
+    timeIn: "00:39.46",
+    duration: 4020
+  },
+  {
+    text: "Bury it",
+    timeIn: "00:43.86",
+    duration: 3000
+  },
+  {
+    text: "I won't let you bury it",
+    timeIn: "00:45.46",
+    duration: 3960
+  },
+  {
+    text: "I won't let you smother it",
+    timeIn: "00:49.78",
+    duration: 3980
+  },
+  {
+    text: "I won't let you murder it",
+    timeIn: "00:53.76",
+    duration: 3940
+  },
+  {
+    text: "Our time is running out",
+    timeIn: "00:57.10",
+    duration: 4020
+  },
+  {
+    text: "And our time is running out",
+    timeIn: "01:01.62",
+    duration: 4630
+  },
+  {
+    text: "You can't push it underground",
+    timeIn: "01:05.45",
+    duration: 4510
+  },
+  {
+    text: "We can't stop it screaming out",
+    timeIn: "01:09.96",
+    duration: 5060
+  },
+  {
+    text: "",
+    timeIn: "01:14.00",
+    duration: 8460
+  },
+  {
+    text: "I wanted freedom",
+    timeIn: "01:23.42",
+    duration: 1960
+  },
+  {
+    text: "But I'm restricted",
+    timeIn: "01:25.38",
+    duration: 1860
+  },
+  {
+    text: "I tried to give you up",
+    timeIn: "01:27.54",
+    duration: 2660
+  },
+  {
+    text: "But I'm addicted",
+    timeIn: "01:29.90",
+    duration: 1990
+  },
+  {
+    text: "Now that you know I'm trapped",
+    timeIn: "01:01.89",
+    duration: 420
+  },
+  {
+    text: "Sense of elation",
+    timeIn: "01:02.31",
+    duration: 4730
+  },
+  {
+    text: "You'll never dream of breaking this fixation",
+    timeIn: "01:07.04",
+    duration: 6000
+  },
+  {
+    text: "You will squeeze the life out of me",
+    timeIn: "01:13.04",
+    duration: 4240
+  },
+  {
+    text: "Bury it",
+    timeIn: "01:17.28",
+    duration: 4070
+  },
+  {
+    text: "I won't let you bury it",
+    timeIn: "01:21.35",
+    duration: 1790
+  },
+  {
+    text: "I won't let you smother it",
+    timeIn: "01:23.14",
+    duration: 6220
+  },
+  {
+    text: "I won't let you murder it",
+    timeIn: "01:29.36",
+    duration: 5840
+  },
+  {
+    text: "There's no guarantee, it's not up to me, we can only see",
+    timeIn: "01:35.20",
+    duration: 6210
+  },
+  {
+    text: "This is my legacy, legacy, legacy, legacy",
+    timeIn: "01:41.41",
+    duration: 5540
+  },
+  {
+    text: "[Verse 2]",
+    timeIn: "01:46.95",
+    duration: 1040
+  },
+  {
+    text:
+      "I used to be the type of kid that would always think the sky is fallin'",
+    timeIn: "01:47.99",
+    duration: 3380
+  },
+  {
+    text: "Why am I so differently wired in my noggin?",
+    timeIn: "01:51.37",
+    duration: 3020
+  },
+  {
+    text: "Cause sporadic as my thoughts come, it's mind-bogglin'",
+    timeIn: "01:54.39",
+    duration: 2680
+  },
+  {
+    text: "Cause I obsess on everything in my mind",
+    timeIn: "01:57.07",
+    duration: 2630
+  },
+  {
+    text: "Small shit bothers me, but not my father",
+    timeIn: "01:59.70",
+    duration: 3190
+  },
+  {
+    text: "He said sayonara then split but I don’t give a shit",
+    timeIn: "02:02.89",
+    duration: 1870
+  },
+  {
+    text: "I'm fine long as there's batteries in my Walkman",
+    timeIn: "02:04.76",
+    duration: 3100
+  },
+  {
+    text: "Nothing is the matter with me",
+    timeIn: "02:07.86",
+    duration: 1330
+  },
+  {
+    text: "Shit look on the bright side at least I ain't walkin'",
+    timeIn: "02:09.19",
+    duration: 2570
+  },
+  {
+    text: "I bike ride through the neighborhood of my apartment",
+    timeIn: "02:11.76",
+    duration: 2790
+  },
+  {
+    text: "Complex on a ten-speed which I've acquired parts that",
+    timeIn: "02:14.55",
+    duration: 2960
+  },
+  {
+    text: "I find in the garbage, a frame then put tires on it",
+    timeIn: "02:17.51",
+    duration: 2960
+  },
+  {
+    text: "Headphones on, look straight ahead if kids try and start shit",
+    timeIn: "02:20.47",
+    duration: 2980
+  },
+  {
+    text: "But if this is all there is for me, life offers",
+    timeIn: "02:23.45",
+    duration: 2100
+  },
+  {
+    text: "Why bother even trying to put up a fight? It's nonsense",
+    timeIn: "02:25.55",
+    duration: 3730
+  },
+  {
+    text: "But I think a light bulb just lit up in my conscience",
+    timeIn: "02:29.28",
+    duration: 2970
+  },
+  {
+    text: "What about them rhymes I've been jottin'",
+    timeIn: "02:32.25",
+    duration: 2120
+  },
+  {
+    text: "They are kind of giving me confidence",
+    timeIn: "02:34.37",
+    duration: 1280
+  },
+  {
+    text: "Instead of trying to escape through my comics",
+    timeIn: "02:35.65",
+    duration: 2380
+  },
+  {
+    text: "Why don't I just blast a little something, like Onyx",
+    timeIn: "02:38.03",
+    duration: 2980
+  },
+  {
+    text: "To put me in the mood to wanna fight and write songs that",
+    timeIn: "02:41.01",
+    duration: 2900
+  },
+  {
+    text: "Say what I want to say to the kid that said that I eyeballed him",
+    timeIn: "02:43.91",
+    duration: 2980
+  },
+  {
+    text: "Grab hold of my balls like that's right, fight's on bitch",
+    timeIn: "02:46.89",
+    duration: 2930
+  },
+  {
+    text: "Who woulda knew from the moment I turned the mic on that",
+    timeIn: "02:49.82",
+    duration: 2870
+  },
+  {
+    text: "I could be iconic, in my conquest",
+    timeIn: "02:52.69",
+    duration: 2680
+  },
+  {
+    text: "That's word to Phife Dawg from a Tribe Called Quest",
+    timeIn: "02:55.37",
+    duration: 2160
+  },
+  {
+    text: "[Hook: Polina]",
+    timeIn: "02:57.53",
+    duration: 500
+  },
+  {
+    text: "Cause this is my legacy, legacy, yeah",
+    timeIn: "02:58.03",
+    duration: 5620
+  },
+  {
+    text: "This is my legacy, legacy, yeah",
+    timeIn: "03:03.65",
+    duration: 5370
+  },
+  {
+    text: "There's no guarantee, it's not up to me, we can only see",
+    timeIn: "03:09.02",
+    duration: 6010
+  },
+  {
+    text: "This is my legacy, legacy, legacy, legacy",
+    timeIn: "03:15.03",
+    duration: 5860
+  },
+  {
+    text: "[Verse 3]",
+    timeIn: "03:20.89",
+    duration: 530
+  },
+  {
+    text:
+      "I used to be the type of kid that would always think the sky is fallin'",
+    timeIn: "03:21.42",
+    duration: 3460
+  },
+  {
+    text: "Now I think the fact that I'm differently wired's awesome",
+    timeIn: "03:24.88",
+    duration: 2800
+  },
+  {
+    text: "Cause if I wasn't, I wouldn't be able to work words like this",
+    timeIn: "03:27.68",
+    duration: 3500
+  },
+  {
+    text: "And connect lines like crosswords",
+    timeIn: "03:31.18",
+    duration: 2130
+  },
+  {
+    text: "And use my enemy's words as strength",
+    timeIn: "03:33.31",
+    duration: 1910
+  },
+  {
+    text: "To try and draw from, and get inspired off 'em",
+    timeIn: "03:35.22",
+    duration: 2830
+  },
+  {
+    text: "Cause all my life I was told and taught I am not shit",
+    timeIn: "03:38.05",
+    duration: 2850
+  },
+  {
+    text: "By you wack fucking giant sacks of lying dog shit",
+    timeIn: "03:40.90",
+    duration: 3350
+  },
+  {
+    text: "Now you shut up bitch, I am talkin' thought I was full of horseshit",
+    timeIn: "03:44.25",
+    duration: 3810
+  },
+  {
+    text: "And now you fucking worship the ground on which I am walkin'",
+    timeIn: "03:48.06",
+    duration: 3100
+  },
+  {
+    text: "Me against the world, so what? I'm Brian Dawkins",
+    timeIn: "03:51.16",
+    duration: 3000
+  },
+  {
+    text: "Versus the whole 0-16 Lions offense",
+    timeIn: "03:54.16",
+    duration: 2870
+  },
+  {
+    text: "So bring on the Giants, Falcons and Miami Dolphins",
+    timeIn: "03:57.03",
+    duration: 2830
+  },
+  {
+    text: "It's the body bag game, bitch, I'm supplyin' coffins",
+    timeIn: "03:59.86",
+    duration: 2820
+  },
+  {
+    text: "Cause you dicks butt kiss, bunch of Brian Baldingers",
+    timeIn: "04:02.68",
+    duration: 3700
+  },
+  {
+    text: "You're going to die a ball licker, I've been diabolical",
+    timeIn: "04:06.38",
+    duration: 2500
+  },
+  {
+    text: "With this dialogue since '99 Rawkus",
+    timeIn: "04:08.88",
+    duration: 3150
+  },
+  {
+    text: "You don't respect the legacy I leave behind y'all can suck a dick",
+    timeIn: "04:12.03",
+    duration: 3330
+  },
+  {
+    text: "The day you beat me, pigs'll fly out my ass",
+    timeIn: "04:15.36",
+    duration: 2880
+  },
+  {
+    text: "In a flying saucer full of Italian sausage",
+    timeIn: "04:18.24",
+    duration: 2620
+  },
+  {
+    text: "The most high exalting and I ain't haltin'",
+    timeIn: "04:20.86",
+    duration: 2500
+  },
+  {
+    text: "'til I die of exhaustion, inhale my exhaust fumes",
+    timeIn: "04:23.36",
+    duration: 1620
+  },
+  {
+    text: "The best part about me is I am not you",
+    timeIn: "04:24.98",
+    duration: 5070
+  },
+  {
+    text: "I'm me, I'm the Fire Marshall and this is my...",
+    timeIn: "04:30.05",
+    duration: 2550
+  },
+  {
+    text: "[Hook: Polina]",
+    timeIn: "04:32.60",
+    duration: 1060
+  },
+  {
+    text: "Cause this is my legacy, legacy, yeah",
+    timeIn: "04:33.66",
+    duration: 3080
+  },
+  {
+    text: "This is my legacy, legacy, yeah",
+    timeIn: "04:36.74",
+    duration: 6320
+  },
+  {
+    text: "There's no guarantee, it's not up to me, we can only see",
+    timeIn: "04:43.06",
+    duration: 5880
+  },
+  {
+    text: "This is my legacy, legacy, legacy, legacy",
+    timeIn: "04:48.94",
+    duration: 7800
+  },
+  {
+    text: "--- www.LRCgenerator.com ---",
+    timeIn: "04:56.74"
+  }
+];
+
+var player = new Vue({
+  el: "#player",
+  data: {
+    lyrics: {
+      show: false,
+      timer: null,
+      currentLine: "",
+      text: lyricsText
+    },
+
+    song: {
+      artist: "MUSE",
+      title: "Time is Running Out",
+      progres: 0
+    },
+
+    audio: null,
+
+    playing: false,
+
+    playerTouched: false
+  },
+
+  ready: function() {
+    // Setup the audio element
+    this.audio = this.$els.audio;
+
+    // Setup time tracker updates
+    this.audio.addEventListener("timeupdate", this.updateTimeTracker, false);
+
+    // Setup end reset
+    this.audio.addEventListener("ended", this.resetPlayer, false);
+  },
+
+  methods: {
+    hideText: function() {
+      this.lyrics.show = !this.lyrics.show;
+    },
+
+    startPlayback: function() {
+      this.playing = true;
+
+      this.audio.volume = 0.5;
+      this.audio.play();
+
+      this.lyrics.show = true;
+      this.displayNextLine(0);
+    },
+
+    togglePlayback: function() {
+      if (!this.playerTouched) {
+        this.startPlayback();
+        this.playerTouched = true;
+        return;
+      }
+      this.playing = !this.playing;
+
+      if (this.audio.paused) {
+        this.audio.play();
+        this.resumeLyrics();
+      } else {
+        this.audio.pause();
+        this.pauseLyrics();
+      }
+    },
+
+    pauseLyrics: function() {
+      if (this.lyrics.timer) {
+        this.lyrics.timer.pause();
+      }
+    },
+
+    resumeLyrics: function() {
+      if (this.lyrics.timer) {
+        this.lyrics.timer.resume();
+      }
+    },
+
+    displayNextLine: function(line) {
+      var _this = this;
+
+      // Set the currentLine, which will auto display
+      _this.lyrics.currentLine = _this.lyrics.text[line].text;
+
+      if (_this.lyrics.text[line + 1] !== undefined) {
+        _this.lyrics.timer = new Timer(function() {
+          _this.displayNextLine(line + 1);
+        }, _this.lyrics.text[line].duration);
+      } else {
+        _this.lyrics.currentLine = "THE END";
+      }
+    },
+
+    updateTimeTracker: function() {
+      this.song.progress = 100 * (this.audio.currentTime / this.audio.duration);
+    },
+
+    resetPlayer: function() {
+      this.playing = false;
+      this.playerTouched = false;
+    }
+  }
+});
+
+function Timer(callback, delay) {
+  var timerId;
+  var start;
+  var remaining = delay;
+
+  this.pause = function() {
+    window.clearTimeout(timerId);
+    remaining -= new Date() - start;
+  };
+
+  this.resume = function() {
+    start = new Date();
+    window.clearTimeout(timerId);
+    timerId = window.setTimeout(callback, remaining);
+  };
+  this.resume();
+}
+
+// Reference: https://codepen.io/tuannvbg/pen/ZBrrjr?editors=0010
